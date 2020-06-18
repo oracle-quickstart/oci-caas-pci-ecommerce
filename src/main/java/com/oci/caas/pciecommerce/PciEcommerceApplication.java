429a0cbec8df5ee42260a02b407c0ef2c9de9910
Arjun Patel <arjun.patel@oracle.com>
2020-06-18 17:04:02 -0400

    inital commit

---
+++ src/main/java/com/oci/caas/pciecommerce/PciEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.oci.caas.pciecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PciEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PciEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oci/caas/pciecommerce/PciEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oci.caas.pciecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PciEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
