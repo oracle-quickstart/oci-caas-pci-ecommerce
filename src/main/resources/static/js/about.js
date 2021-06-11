d25d3d80c10d90a5f3f90704694e7148a7ec6deb
Yupei Yang <yupei.yang@oracle.com>
2021-06-11 15:53:33 -0700

    Update the about page based on the dev team's comments

--- src/main/resources/static/js/about.js
+++ src/main/resources/static/js/about.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     async function getVersion() {
         try {
             let result = await fetch('/version');
-            console.log(result)
             return result.json();
         } catch (e) {
             console.log(e);
