e37debec66f64e3da2b3582a09d7b5a3cc6f7c62
sskarnaw <sakshi.karnawat@oracle.com>
2021-07-15 13:32:32 -0700

    Adding proper file path for pom.xml

--- src/main/java/com/oci/caas/pciecommerce/rest/AboutPageRestController.java
+++ src/main/java/com/oci/caas/pciecommerce/rest/AboutPageRestController.java
@@ -10,6 +10,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class AboutPageRestController {
@@ -22,13 +25,26 @@ public class AboutPageRestController {
         if (new File("pom.xml").exists()) {
             model = reader.read(new FileReader("pom.xml"));
         } else {
-            model = reader.read(
-                    new InputStreamReader(
-                            AboutPageRestController.class.getResourceAsStream(
-                                    "/META-INF/maven/pom.xml"
-                            )
-                    )
-            );
+            Path path = Paths.get("/META-INF/maven/pom.xml");
+            if (Files.exists(path)){
+                model = reader.read(
+                        new InputStreamReader(
+                                AboutPageRestController.class.getResourceAsStream(
+                                        "/META-INF/maven/pom.xml"
+                                )
+                        )
+                );
+            }
+            else{
+                model = reader.read(
+                        new InputStreamReader(
+                                AboutPageRestController.class.getResourceAsStream(
+                                        "/opt/tomcat_webapp/webapps/ROOT/META-INF/maven/com.oci.caas/pci-ecommerce/pom.xml"
+                                )
+                        )
+                );
+
+            }
         }
         return new VersionResponse(model.getVersion());
     }
